0cd54b26b1e11234f65c5e0f2c76341a9167e09a
Luis Marques <Luis Marques@Marques-PC>
2013-03-19 15:10:04 +0000

    Grupo 6 - Registo tipo Rendimento

---
+++ eapli.ExpenseManager/src/eapli/bootstrap/IncomeTypeBootStrapper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.bootstrap;
+
+import Model.IncomeType;
+import Persistence.IIncomeTypeRepository;
+import Persistence.IncomeTypeRepository;
+
+/**
+ *
+ * @author Luis Marques
+ */
+public class IncomeTypeBootStrapper {
+    static {
+        IIncomeTypeRepository repo = new IncomeTypeRepository();
+        repo.save(new IncomeType("Alimentacao","Necessidade Basica"));
+        repo.save(new IncomeType("Transportes","Necessidade Basica"));
+        repo.save(new IncomeType("Lazer","Necessidade Secundaria"));
+    }
+}
