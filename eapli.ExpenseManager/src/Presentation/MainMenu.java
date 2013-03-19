789c3f46b34be0bb9a9fc1288fe0fb429aae786e
Luis Marques <Luis Marques@Marques-PC>
2013-03-19 15:22:44 +0000

    Grupo 6 - Alteração do Main Menu com a nova opcao  de Registo de Tipo de Rendimento

--- eapli.ExpenseManager/src/Presentation/MainMenu.java
+++ eapli.ExpenseManager/src/Presentation/MainMenu.java
@@ -11,28 +11,40 @@ import eapli.util.Console;
  * @author Paulo Gandra Sousa
  */
 public class MainMenu {
+
     public void mainLoop() {
-        System.out.println("===================");
-        System.out.println("  EXPENSE MANAGER  ");
-        System.out.println("===================\n");
         
-        System.out.println("1. Register an expense");
-        System.out.println("6. Register new Income Type");
-        System.out.println("0. Exit\n\n");
+        int option;
+        do {
+            System.out.println("===================");
+            System.out.println("  EXPENSE MANAGER  ");
+            System.out.println("===================\n");
         
-        int option = Console.readInteger("Please choose a option");
-        switch (option) {
-            case 0: 
-                System.out.println("bye bye ...");
-                return;
-            case 1: 
-                ExpenseRegisterUI ui = new ExpenseRegisterUI();
-                ui.mainLoop();
-                break;
-            case 6:
-                IncomeTypeRegisterUI ui6 = new IncomeTypeRegisterUI();
-                ui6.mainLoop();
-                break;
-        }
-    }
+            System.out.println("1. Register an expense");
+            System.out.println("2. Register an expense type");
+            System.out.println("6. Register new Income Type");
+            System.out.println("0. Exit\n\n");
+        
+            option = Console.readInteger("Please choose a option");
+            switch (option) {
+                case 0: 
+                    System.out.println("bye bye ...");
+                    return;
+                case 1: 
+                    ExpenseRegisterUI ui = new ExpenseRegisterUI();
+                    ui.mainLoop();
+                    break;
+                case 2:
+                    ExpenseTypeRegisterUI uit = new ExpenseTypeRegisterUI();
+                    uit.mainLoop();
+                    break;
+                case 6:
+                    IncomeTypeRegisterUI ui6 = new IncomeTypeRegisterUI();
+                    ui6.mainLoop();
+                    break;
+            }
+       }while (option != 0);
+
+     }
 }
+        
