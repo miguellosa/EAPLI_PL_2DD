649477c1adfeaf3f7889eaf01365312b0d55cc9d
1080772AntonioDidier <1080772@isep.ipp.pt>
2013-04-03 16:27:51 +0100

    alteração dos cases do MainMenu

--- eapli.ExpenseManager/src/Presentation/MainMenu.java
+++ eapli.ExpenseManager/src/Presentation/MainMenu.java
@@ -14,7 +14,10 @@ public class MainMenu {
 
     public void mainLoop() {
         
-        
+        final int Expense_Register_UI=1;
+        final int Expense_Type_Register_UI=2;
+        final int Payment_Mean_Register_UI=3;
+        final int Income_Type_Register_UI=6;
         
         int option;
         do {
@@ -30,22 +33,22 @@ public class MainMenu {
         
             option = Console.readInteger("Please choose a option");
             switch (option) {
-                case 0: 
+                case 0:         //Exit option 
                     System.out.println("bye bye ...");
                     return;
-                case 1: 
+                case Expense_Register_UI:         //Option to regist an expense into the system
                     ExpenseRegisterUI ui = new ExpenseRegisterUI();
                     ui.mainLoop();
                     break;
-                case 2:
+                case Expense_Type_Register_UI:         //Option to regist an expense type into the system
                     ExpenseTypeRegisterUI uit = new ExpenseTypeRegisterUI();
                     uit.mainLoop();
                     break;
-                case 3:
+                case Payment_Mean_Register_UI:         //Option to regist a mean of payment into the system
                     PaymentMeanRegisterUI ui3= new PaymentMeanRegisterUI();
                     ui3.mainLoop();
                     break;
-                case 6:
+                case Income_Type_Register_UI:         //Option to regist an income type into the system
                     IncomeTypeRegisterUI ui6 = new IncomeTypeRegisterUI();
                     ui6.mainLoop();
                     break;
