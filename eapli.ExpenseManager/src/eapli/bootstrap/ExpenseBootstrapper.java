b1c37794db698718c409fd2f7b26c7bc943e027a
i110722 <i110722@PCI085.dei.isep.ipp.pt>
2013-04-16 13:37:10 +0100

    Grupo 3 - Alterações no construtor da classe Expense

--- eapli.ExpenseManager/src/eapli/bootstrap/ExpenseBootstrapper.java
+++ eapli.ExpenseManager/src/eapli/bootstrap/ExpenseBootstrapper.java
@@ -5,6 +5,7 @@
 package eapli.bootstrap;
 
 import Model.Expense;
+import Model.ExpenseType;
 import Model.PaymentMean;
 import Model.PaymentTypes;
 import Persistence.ExpenseRepository;
@@ -19,9 +20,9 @@ public class ExpenseBootstrapper {
 
     static {
         IExpenseRepository repo = new ExpenseRepository();
-        repo.save(new Expense("Autocarro", 2013, 3, 4, new BigDecimal(2.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO)));
-        repo.save(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO)));
-        repo.save(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO)));
-        repo.save(new Expense("Cinema", 2013, 3, 4, new BigDecimal(5.0), new PaymentMean("O meu cartao", PaymentTypes.CARTAO)));
+        repo.save(new Expense("Autocarro", 2013, 3, 4, new BigDecimal(2.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO),new ExpenseType("Lazer")));
+        repo.save(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO),new ExpenseType("Alimentação")));
+        repo.save(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), new PaymentMean("O meu cartao", PaymentTypes.CARTAO),new ExpenseType("Vestuario")));
+        repo.save(new Expense("Cinema", 2013, 3, 4, new BigDecimal(5.0), new PaymentMean("O meu cartao", PaymentTypes.CARTAO),new ExpenseType("Lazer")));
     }
 }
