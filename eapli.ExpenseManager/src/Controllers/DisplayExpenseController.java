a5cc637d8145fb10f9fea67bb524a26853f49525
soaressf <soaressf@soaressfptmwl>
2013-04-23 14:57:59 +0100

    Grupo 6 - Alteração da classe DisplayExpenseController para conclusão do método getMonthlyExpenseByType que retorna o somatória das despesas por tipo num array de Bigdecimal.

--- eapli.ExpenseManager/src/Controllers/DisplayExpenseController.java
+++ eapli.ExpenseManager/src/Controllers/DisplayExpenseController.java
@@ -4,8 +4,14 @@
  */
 package Controllers;
 
+import Controllers.ExpenseTypeRegisterController;
+import Controllers.ExpenseController;
+import Model.DTOExpenseType;
+import Model.Expense;
 import Model.ExpenseType;
+import Model.RecordExpense;
 import Persistence.ExpenseTypeRepository;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -14,12 +20,22 @@ import java.util.List;
  */
 public class DisplayExpenseController {
     
+    private List<ExpenseType> listExpenseTypes;
+    private List<Expense> listExpenses;
+    
     public DisplayExpenseController() {
     
     }
 
-    public List<ExpenseType> getTypesExpenseList(){
-        ExpenseTypeRepository repo = new ExpenseTypeRepository();
-        return repo.getList();
+    public BigDecimal[] getMonthlyExpensesByType(){
+        ExpenseTypeRegisterController registerController = new ExpenseTypeRegisterController();
+        listExpenseTypes = registerController.GetAllExpenseTypes();
+        
+        ExpenseController expenseController = new ExpenseController();
+        listExpenses =  expenseController.getExpenseList();
+        
+        DTOExpenseType dto = new DTOExpenseType(listExpenseTypes, listExpenses);
+        RecordExpense rce = new RecordExpense(dto);
+        return rce.getMonthlyExpensesByType();
     }
 }
