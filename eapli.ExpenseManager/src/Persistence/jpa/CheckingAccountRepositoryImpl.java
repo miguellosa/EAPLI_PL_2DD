6add43a30318ce443efe13f85ef1081da9637cc8
1110311RuiSilva <rui.m.a.s@hotmail.com>
2013-05-21 15:00:24 +0100

    Grupo 2 ->criação da classe CheckingAccountRepositoryImpl

---
+++ eapli.ExpenseManager/src/Persistence/jpa/CheckingAccountRepositoryImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistence.jpa;
+
+import Model.CheckingAccount;
+
+/**
+ *
+ * @author i110311
+ */
+public class CheckingAccountRepositoryImpl extends JpaRepository<CheckingAccount, String> {
+    
+}
