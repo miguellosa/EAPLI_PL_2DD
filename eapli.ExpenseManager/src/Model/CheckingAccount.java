977359f186b45efe077ef257da243cd5ad64e68c
i110815 <i110815@PCI087.dei.isep.ipp.pt>
2013-05-21 14:54:16 +0100

    Completei a classe CheckingAccount

--- eapli.ExpenseManager/src/Model/CheckingAccount.java
+++ eapli.ExpenseManager/src/Model/CheckingAccount.java
@@ -6,11 +6,12 @@ package Model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
-import javax.naming.InsufficientResourcesException;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -31,25 +32,25 @@ public class CheckingAccount extends Observable implements Serializable {
     @Id
     @GeneratedValue
     private long id;
-//    
-//    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
-//    @JoinTable(name = "CheckingAccount_Movements")
-//    private List<Movement> movements;
+////    
+////    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+////    @JoinTable(name = "CheckingAccount_Movements")
+////    private List<Movement> movements;
 //    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
 //    @JoinTable(name = "CheckingAccount_Expenses")
-//    private List<Movement> expenses;
+//    private List<Expense> expenses;
 //    @OneToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
 //    @JoinTable(name = "CheckingAccount_Incomes")
-//    private List<Movement> incomes;
-//    @OneToOne(cascade = CascadeType.MERGE)
-//    private InitialBalance initialBalance;
-//    
-//    @Transient
-//    private Map<ExpenseType, List<Expense>> expensesByType;
-//    
-//    public CheckingAccount() {
-//            expensesByType = new HashMap<ExpenseType, List<Expense>>();
-//    }
+//    private List<Income> incomes;
+////    @OneToOne(cascade = CascadeType.MERGE)
+////    private InitialBalance initialBalance;
+    
+    @Transient
+    private Map<ExpenseType, List<Expense>> expensesByType;
+    
+    public CheckingAccount() {
+            expensesByType = new HashMap<ExpenseType, List<Expense>>();
+    }
     
 //    public boolean hasId(){
 //        return id != null;
@@ -62,10 +63,10 @@ public class CheckingAccount extends Observable implements Serializable {
 //    public BigDecimal totalEarnings(){
 //        return sumAmount(getIncomes());
 //    }
-    
+//    
 //    public void registerIncome(Income income){
 //        addMovement(income);
-//        classifyMovemetAsIncome(income);
+//        classifyMovementAsIncome(income);
 //    }
     
 //    public BigDecimal sumAmount(List<? extends Movement> theMovements){
@@ -75,7 +76,7 @@ public class CheckingAccount extends Observable implements Serializable {
 //        }
 //        return sum;
 //    }
-    
+//    
 //    public void registerExpense(Expense expense) throws InsufficientBalanceException{
 //        if(!hasEnoughBalance(expense.getAmount())){
 //            throw new InsufficientBalanceException();
@@ -87,22 +88,68 @@ public class CheckingAccount extends Observable implements Serializable {
 //        ExpenseRegisteredEvent expenseRegisteredEvent = new ExpenseRegisteredEvent(expense);
 //        this.notifyObservers(expenseRegisteredEvent);
 //    }
-    
+//    
 //    public void registerSavingDeposit(SavingDeposit savingDeposit) throws InsufficientBalanceException{
 //        if(!hasEnoughBalance(savingDeposit.getAmount())){
 //            //throw new InsufficientBalanceException();
 //        }
 //        addMovement(savingDeposit);
 //    }
-    
+//    
 //    public void registerSavingWithdraw(SavingWithdraw savingWithdraw){
 //        addMovement(savingWithdraw);
 //    }
-//    
-//    private void classifyMovementExpense(Expense expense){
-//        expenses.add(expense);
+    
+//  
+//    private void reClassifyMovements(){
+//        for(Movement movement : getMovements()){
+//            if(movement instanceof Expense){
+//                classifyExpense((Expense) movement);
+//            }
+//        }
 //    }
     
+//    public Map<ExpenseType, List<Expense>> getExpensesClassifiedByExpenseType(){
+//        reClassifyMovements();
+//        return Collections.unmodifiableMap(expensesByType);
+//    }
+    
+//    private void addMovement(Movement movement) throws IllegalArgumentException{
+//        if(movement == null){
+//            throw new IllegalArgumentException();
+//        }
+//        
+//        movements.add(movement);
+//    }
+    
+//    private boolean hasEnoughBalance(BigDecimal amount){
+//        if(amount.compareTo(getBalance()) == 1){
+//            return false;
+//        }
+//        
+//        return true;
+//    }
+    
+//    public BigDecimal getBalance() {
+//        BigDecimal i = new BigDecimal(0);
+//        if(initialBalance != null){
+//            i = initialBalance.getValue();
+//        }
+//        
+//        return totalEarnings().subtract(totalExpenditure()).add(i);
+//    }
+    
+//    public void registerInitialBalance(InitialBalance initial){
+//        if(initial == null || initialBalance != null){
+//            throw new IllegalArgumentException();
+//        }
+//        
+//        initialBalance = initial;
+//    }
+    
+//    public boolean hasInitialBalance(){
+//        return initialBalance != null;
+//    }
 
     
 }
